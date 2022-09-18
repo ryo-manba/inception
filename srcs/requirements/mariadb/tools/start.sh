fe72f036112b4e6ae7041f42536c87472385857e
Ryo <matsukawa.ryo42@gmail.com>
2022-09-18 12:49:37 +0900

    update: Better to use mysql_install_db to initialize

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-# https://qiita.com/wakayama-y/items/a9b7380263da77e51711
 if [ ! -e '.done' ]; then
     echo "[INFO] start mysql initialization."
-    mysqld_safe &
+    mysql_install_db --user=mysql
 
-    # mysqlが起動したかチェックする
-    echo "[DEBUG] start ping"
-    mysqladmin --wait --count 3 ping
+    echo "[INFO] create database."
+    mysqld -u mysql --bootstrap << EOF
+    CREATE DATABASE ${WP_DB_NAME};
+    CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';
+    GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';
+    FLUSH PRIVILEGES;
+EOF
 
-    echo "[DEBUG] create database"
-    mysql -e "CREATE DATABASE ${WP_DB_NAME};"
-    mysql -e "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';"
-    mysql -e "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';"
-    mysql -e "FLUSH PRIVILEGES;"
-
-    echo "[DEBUG] shutdown mysql"
-    mysqladmin shutdown
     touch .done
+    echo "[INFO] initial setup is done."
 else
     echo "[INFO] mysql initialization already done."
 fi
-echo "[DEBUG] initial setup is done."
+
+echo "[INFO] MariaDB started."
 mysqld_safe
