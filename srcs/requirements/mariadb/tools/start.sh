b41e3ed177281682422c75b81508ba51f2cff414
Ryo <matsukawa.ryo42@gmail.com>
2022-09-18 15:39:43 +0900

    revert: not working

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -2,21 +2,19 @@
 
 if [ ! -e '.done' ]; then
     echo "[INFO] start mysql initialization."
-    mysql_install_db --user=mysql
+    mysqld_safe &
 
-    echo "[INFO] create database."
-    mysqld -u mysql --bootstrap << EOF
-    CREATE DATABASE ${WP_DB_NAME};
-    CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';
-    GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';
-    FLUSH PRIVILEGES;
-EOF
+    echo "[INFO] create database"
+    mysql -e "CREATE DATABASE ${WP_DB_NAME};"
+    mysql -e "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';"
+    mysql -e "FLUSH PRIVILEGES;"
 
+    mysqladmin shutdown
     touch .done
     echo "[INFO] initial setup is done."
 else
     echo "[INFO] mysql initialization already done."
 fi
-
 echo "[INFO] MariaDB started."
 mysqld_safe
