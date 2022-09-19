40b500a18ec6dd6f6de848a990091cae915fe9d3
Ryo <matsukawa.ryo42@gmail.com>
2022-09-19 14:40:33 +0900

    update: execute only at first startup

--- srcs/requirements/nginx/tools/start.sh
+++ srcs/requirements/nginx/tools/start.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 
-# 設定ファイル
-ln -s /etc/nginx/sites-available/inception.conf /etc/nginx/sites-enabled/inception.conf
-# 元々存在するファイルは不要なので削除する
-rm -rf /etc/nginx/sites-enabled/default
-
-# SSLの設定
-mkdir /etc/nginx/ssl; \
-openssl req \
-        -x509 \
-        -sha256 \
-        -newkey rsa:2048 \
-        -days 365 \
-        -nodes \
-        -out /etc/nginx/ssl/nginx.crt \
-        -keyout /etc/nginx/ssl/nginx.key \
-        -subj "/C=JP/ST=Tokyo/L=Minato/O=42Tokyo/OU=Student/CN=localhost"
-
 # foregroudで起動する
 nginx -g "daemon off;"
