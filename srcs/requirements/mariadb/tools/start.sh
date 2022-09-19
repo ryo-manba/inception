fad20cdf89680362318e7e7a303ef006cc2cc82c
Ryo <matsukawa.ryo42@gmail.com>
2022-09-19 16:07:13 +0900

    add: setting mysql root pass

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -11,6 +11,7 @@ if [ ! -e '.done' ]; then
 
     queryfile=$(mktemp)
     cat << EOF > $queryfile
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';
     CREATE DATABASE ${WP_DB_NAME};
     CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';
     GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';
