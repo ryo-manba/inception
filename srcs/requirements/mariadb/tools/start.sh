9b81c755b54a50c9bd450504b495338cbfad4eed
Ryo <matsukawa.ryo42@gmail.com>
2022-09-19 13:58:35 +0900

    fix: SQL query executed before mysql is started

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -4,11 +4,16 @@ if [ ! -e '.done' ]; then
     echo "[INFO] start mysql initialization."
     mysqld_safe &
 
-    echo "[INFO] create database"
+    until mysqladmin ping -h --silent; do
+      echo "[INFO] waiting for mysqld to be connectable..."
+      sleep 2
+    done
+
     mysql -e "CREATE DATABASE ${WP_DB_NAME};"
     mysql -e "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';"
     mysql -e "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';"
     mysql -e "FLUSH PRIVILEGES;"
+    echo "[INFO] database created."
 
     mysqladmin shutdown
     touch .done
@@ -16,5 +21,6 @@ if [ ! -e '.done' ]; then
 else
     echo "[INFO] mysql initialization already done."
 fi
+
 echo "[INFO] MariaDB started."
 mysqld_safe
